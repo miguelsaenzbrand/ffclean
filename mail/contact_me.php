59d99f437cbb6ffdf95c355e1e3ad5500cf02076
miguelsaenzbrand <miguelsaenz@Miguels-Mac-Pro.local>
2017-03-21 23:13:31 -0300

    Final ffClean

---
+++ mail/contact_me.php
@@ -0,0 +1,24 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+
+// Create the email and send the message
+$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Contacto web:  $name";
+$email_body = "Recibió un nuevo mensaje de su formulario desde el sitio web.\n\n"."Detalles del contato:\n\nNombre: $name\n\nEmail: $email_address\n\nMensaje:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Responder a: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;
+?>
