88275cb723acbd55c70caf0879b2853d12ea3859
miguelsaenzbrand <miguelsaenz@Miguels-Mac-Pro.local>
2017-04-25 10:04:24 -0300

    actualizacion mail php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,7 +17,7 @@ $message = $_POST['message'];
 $to = 'info@ffclean.co'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contacto web:  $name";
 $email_body = "Recibió un nuevo mensaje de su formulario desde el sitio web.\n\n"."Detalles del contacto:\n\nNombre: $name\n\nEmail: $email_address\n\nMensaje:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@ffclean.co\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Responder a: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
