97d4c2459f7d0eaeb64cff0bbb5dc235db0f2aea
miguelsaenzbrand <miguelsaenz@Miguels-Mac-Pro.local>
2017-04-25 09:46:01 -0300

    mail en el formulario php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,11 +14,11 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'info@ffclean.co'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contacto web:  $name";
-$email_body = "Recibió un nuevo mensaje de su formulario desde el sitio web.\n\n"."Detalles del contato:\n\nNombre: $name\n\nEmail: $email_address\n\nMensaje:\n$message";
+$email_body = "Recibió un nuevo mensaje de su formulario desde el sitio web.\n\n"."Detalles del contacto:\n\nNombre: $name\n\nEmail: $email_address\n\nMensaje:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Responder a: $email_address";	
+$headers .= "Responder a: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
 ?>
